80e448a6484e4647f97ef93342097b09ef9254ae
Reiner Tupaz <rstupaz@hotmail.com>
2024-05-05 21:22:17 +0800

    Inital Commit

---
+++ Bill Manager/AddBillerActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.Content;
+
+namespace Bill_Manager;
+
+[Activity(Label = "@string/add_biller_title")]
+public class AddBillerActivity : Activity
+{
+    Button btnAddBiller;
+    EditText etxtTitle;
+    EditText etxtAcctName;
+    EditText etxtAcctNo;
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        SetContentView(Resource.Layout.activity_addbiller);
+
+        btnAddBiller = (Button)FindViewById(Resource.Id.btnAddBiller);
+        etxtTitle = (EditText)FindViewById(Resource.Id.etxtTitle);
+        etxtAcctName = (EditText)FindViewById(Resource.Id.etxtAcctName);
+        etxtAcctNo = (EditText)FindViewById(Resource.Id.etxtAcctNo);
+
+        btnAddBiller.Click += BtnAddBiller_Click;
+    }
+
+    private void BtnAddBiller_Click(object? sender, EventArgs e)
+    {
+        if (ValidateFields())
+        {
+            //TODO: Add saving logic
+
+            var i = new Intent();
+            SetResult(Result.Ok, i);
+            Finish();
+        }
+        else
+            Toast.MakeText(this, Resource.String.toast_incomplete_fields, ToastLength.Short).Show();
+    }
+
+    private bool ValidateFields()
+    {
+        bool isValid = true;
+
+        if (string.IsNullOrWhiteSpace(etxtTitle.Text))
+            isValid = false;
+        if (string.IsNullOrWhiteSpace(etxtAcctName.Text))
+            isValid = false;
+        if (string.IsNullOrWhiteSpace(etxtAcctNo.Text))
+            isValid = false;
+
+        return isValid;
+    }
+}
---
+++ Bill Manager/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.Content;
+using Android.Runtime;
+
+namespace Bill_Manager;
+
+[Activity(Label = "@string/home_title", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    public static int ADD_BILLER = 0;
+
+    Button btnAddBiller;
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+
+        btnAddBiller = (Button)FindViewById(Resource.Id.btnAddBiller);
+        btnAddBiller.Click += BtnAddBiller_Click;
+    }
+
+    private void BtnAddBiller_Click(object? sender, EventArgs e)
+    {
+        var i = new Intent(this, typeof(AddBillerActivity));
+        StartActivityForResult(i, ADD_BILLER);
+    }
+
+    protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent? data)
+    {
+        base.OnActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_BILLER && resultCode == Result.Ok)
+        {
+            //TODO: Refresh recyclerview here!
+        }
+    }
+}
